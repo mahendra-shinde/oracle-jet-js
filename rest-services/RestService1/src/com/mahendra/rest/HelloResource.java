47cde8238a9296f13edba2865c711d6d3ae7d835
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-09-24 17:23:55 +0530

    REST Demo updated

--- rest-services/RestService1/src/com/mahendra/rest/HelloResource.java
+++ rest-services/RestService1/src/com/mahendra/rest/HelloResource.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.mahendra.models.Account;
+
 @Path("hello")
 public class HelloResource {
 
@@ -17,5 +19,10 @@ public class HelloResource {
 	public String sayHello2() {
 		System.out.println("Invoking HTML");
 		return "<h2>Hello World</h2>";
+	}
+	
+	@GET @Produces({"application/json","application/xml"})
+	public Account getaccount() {
+		return new Account(1001, "Robert Downy Jr",129900D);
 	}
 }
